56a47ab048ea2e103899ad2d32afdf9b5c4b6f82
ast <ast@1xinternet.de>
2017-03-08 17:57:15 +0100

    some content added and templates for nomination-content-type the nomination-template for the full-view needs to be worked on

--- themes/splashawards/public/js/bundle.js
+++ themes/splashawards/public/js/bundle.js
@@ -53,19 +53,32 @@
 
 	(function ($, Drupal) {
 
-	  /*Drupal.behaviors.exampleBehavior = {
-	    attach: (context) => {
-	      $('.my-selector', context).once('example-behavior').each(() => {});
-	    }
-	  };*/
+	    /*Drupal.behaviors.exampleBehavior = {
+	     attach: (context) => {
+	     $('.my-selector', context).once('example-behavior').each(() => {});
+	     }
+	     };*/
 
-	  jQuery('.sponsor-slider').each(function (position, element) {
-	    $(element).slick({
-	      infinite: true,
-	      slidesToShow: 3,
-	      slidesToScroll: 3
+	    jQuery('.sponsor-slider').each(function (position, element) {
+	        $(element).slick({
+	            infinite: true,
+	            slidesToShow: 3,
+	            slidesToScroll: 3
+	        });
 	    });
-	  });
+
+	    jQuery('.nomination-screenshots-slider').each(function (position, element) {
+	        $(element).slick({
+	            infinite: true,
+	            slidesToShow: 1,
+	            slidesToScroll: 1,
+	            swipe: false,
+	            touchMove: false,
+	            autoplay: true,
+	            arrows: false,
+	            autoplaySpeed: 2000
+	        });
+	    });
 	})(jQuery, window.Drupal);
 
 /***/ },
--- themes/splashawards/source/js/main.js
+++ themes/splashawards/source/js/main.js
@@ -5,18 +5,31 @@ const Slick = require('slick-carousel');
 
 (function ($, Drupal) {
 
-  /*Drupal.behaviors.exampleBehavior = {
-    attach: (context) => {
-      $('.my-selector', context).once('example-behavior').each(() => {});
-    }
-  };*/
+    /*Drupal.behaviors.exampleBehavior = {
+     attach: (context) => {
+     $('.my-selector', context).once('example-behavior').each(() => {});
+     }
+     };*/
 
-  jQuery('.sponsor-slider').each(function(position, element) {
-      $(element).slick({
-          infinite: true,
-          slidesToShow: 3,
-          slidesToScroll: 3
-      });
-  });
+    jQuery('.sponsor-slider').each(function (position, element) {
+        $(element).slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+        });
+    });
+
+    jQuery('.nomination-screenshots-slider').each(function (position, element) {
+        $(element).slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            swipe: false,
+            touchMove: false,
+            autoplay: true,
+            arrows: false,
+            autoplaySpeed: 2000
+        });
+    });
 
 })(jQuery, window.Drupal);

1deb11c9374408cb8803a6066164d3369f2059c2
ast <ast@1xinternet.de>
2017-03-10 09:24:27 +0100

    fixed bug
    * javascript reordering and update of DOM-selectors

--- themes/splashawards/source/js/main.js
+++ themes/splashawards/source/js/main.js
@@ -6,17 +6,38 @@ const Slick = require('slick-carousel');
 (function ($, Drupal) {
     Drupal.behaviors.splashawards = {
         attach: function (context, settings) {
-            if (jQuery(context).find('#views-exposed-form-nomination-list-block-2 select').length > 0) {
+            jQuery(context).find('.sponsor-slider').each(function (position, element) {
+                $(element).slick({
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                });
+            });
+
+            jQuery(context).find('.nomination-screenshots-slider').each(function (position, element) {
+                $(element).slick({
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    swipe: false,
+                    touchMove: false,
+                    autoplay: true,
+                    arrows: false,
+                    autoplaySpeed: 2000
+                });
+            });
+
+            if (jQuery(context).find('#block-views-block-compositstartpage-block-2 select').length > 0) {
                 var $menu = jQuery('<ul>'),
                     dataKey = 'category-tid',
-                    $submitButton = jQuery(context).find('#views-exposed-form-nomination-list-block-2 .js-form-submit'),
+                    $submitButton = jQuery(context).find('#block-views-block-compositstartpage-block-2 .js-form-submit'),
                     menuItemSelectedClass = 'selected';
 
                 $menu.toggleClass('nomination-category-menu js-nomination-category-menu');
-                jQuery(context).find('#views-exposed-form-nomination-list-block-2 select').parent().append($menu);
+                jQuery(context).find('#block-views-block-compositstartpage-block-2 select').parent().append($menu);
 
                 // reading the select-options creating the menu-list based on the their content
-                jQuery(context).find('#views-exposed-form-nomination-list-block-2 select option').each(function (position, element) {
+                jQuery(context).find('#block-views-block-compositstartpage-block-2 select option').each(function (position, element) {
                     var tid = element.value,
                         categoryName = element.innerText,
                         $listElement = jQuery('<li>');
@@ -31,27 +52,6 @@ const Slick = require('slick-carousel');
                     $listElement.data(dataKey, tid);
                 });
 
-                jQuery(context).find('.sponsor-slider').each(function (position, element) {
-                    $(element).slick({
-                        infinite: true,
-                        slidesToShow: 3,
-                        slidesToScroll: 3
-                    });
-                });
-
-                jQuery(context).find('.nomination-screenshots-slider').each(function (position, element) {
-                    $(element).slick({
-                        infinite: true,
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        swipe: false,
-                        touchMove: false,
-                        autoplay: true,
-                        arrows: false,
-                        autoplaySpeed: 2000
-                    });
-                });
-
                 // attaching event-listener to rendered out menue
                 jQuery(context).find('.js-nomination-category-menu li').on('click', function(event) {
                     jQuery(event.target).siblings('.' + menuItemSelectedClass).toggleClass(menuItemSelectedClass);
@@ -59,7 +59,7 @@ const Slick = require('slick-carousel');
 
                     // finding the requested option-and selecting it
                     jQuery(context).find(
-                        '#views-exposed-form-nomination-list-block-2 select option[value=' +
+                        '#block-views-block-compositstartpage-block-2 select option[value=' +
                         jQuery(event.target).data(dataKey) + ']'
                     ).selected();
                     $submitButton.click();
